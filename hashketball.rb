7be7db0e21be0ad2fd155448e6d9560a7136ba1a
Austin Evans <unity7591@gmail.com>
2020-05-05 18:41:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -52,6 +52,39 @@ def game_hash
     blocks: 5, 
     slam_dunks: 12},
       ] },
+    :home { team_name: 'Brooklyn Nets',
+    colors: 'Black, White',
+    players:[{ player_name: 'Alan Anderson',
+    number: 0,
+    shoe: 16,
+    points: 22, 
+    rebounds: 12, 
+    assists: 12, 
+    steals: 3, 
+    blocks: 1, 
+    slam_dunks: 1},
+    
+    {player_name: 'Reggie Evans'
+    number: 30,
+    shoe: 14,
+    points: 12, 
+    rebounds: 12, 
+    assists: 12, 
+    steals: 12, 
+    blocks: 12, 
+    slam_dunks: 7},
+    
+    {player_name: 'Brook Lopez',
+        number: 4,
+    shoe: 18,
+    points: 10, 
+    rebounds: 1, 
+    assists: 1, 
+    steals: 2, 
+    blocks: 7, 
+    slam_dunks: 2},
+      ]
+    
     
   }
 }
