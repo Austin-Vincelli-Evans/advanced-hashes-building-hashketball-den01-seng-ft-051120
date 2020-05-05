07cd0ec6446386e84451f37bb412bb314c705f08
Austin Evans <unity7591@gmail.com>
2020-05-05 18:25:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,6 @@
 def game_hash 
   {
-    :away { team_name: 'Charlotte Hornets', colors: 'Turquoise, Puple', players: [
+    :away { team_name: 'Charlotte Hornets', colors: 'Turquoise, Puple', players: [{
       ] 
     
   }
